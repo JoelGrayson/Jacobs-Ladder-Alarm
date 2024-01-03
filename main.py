f1e635bdf40c801363f2abe005622f86aa30cff7
Joel Grayson <joel@joelgrayson.com>
2024-01-03 10:03:48 -0500

    Pressing button turns off light

--- main.py
+++ main.py
@@ -2,28 +2,30 @@
 
 # ABOUT: This is an executable
 
-from time import sleep
-from gpiozero import InputDevice, OutputDevice, LED
-import requests
+from signal import pause
+from gpiozero import LED, Button
 from ENVIRONMENT_VARIABLES import BRIDGE_IP, USERNAME
+import requests
 
-# Use the 
+
+# Pins
 BUTTON_LED_OUT_GPIO=27
-BUTTON_IN_GPIO=22
-BUTTON_OUT_GPIO=23
-
-button_led=LED(BUTTON_LED_OUT_GPIO)
+BUTTON_SIGNAL_GPIO=22
 
 # Pin 13 (GPIO 27) LED PWR
 # Pin 14 LED GND (always GND)
-# Pin 15 (GPIO 22) Btn in
-# Pin 16 (GPIO 23) Btn out
+# Pin 15 (GPIO 22) Button signal
+# Pin 3 Button GND (always GND)
 
-"""
-From `pinout`
-GPIO27 (13) (14) GND
-GPIO22 (15) (16) GPIO23
-"""
+# From `pinout`
+# GPIO27 (13) (14) GND
+# GPIO22 (15) (16) GPIO23
+
+
+# Controls
+button_led=LED(BUTTON_LED_OUT_GPIO)
+button=Button(BUTTON_SIGNAL_GPIO)
+
 
 def jacobs_ladder(state=False):
     print(f'Turning {"on" if state else "off"} Jacobs Ladder...')
@@ -31,18 +33,10 @@ def jacobs_ladder(state=False):
     print("Sending message to", url)
     requests.put(url, json={"on": state})
 
-def listen_to_button():
-    print('Listening to button...')
-    btn_out=OutputDevice(BUTTON_OUT_GPIO) #for btn to receive input
-    btn_out.on()
-    btn_in=InputDevice(BUTTON_IN_GPIO) #read if button pressed
-    #btn_in.when_pressed=lambda x:jacobs_ladder(False)
-    while True: #Listen for button press
-        if btn_in.is_active: #button pressed bc btn_in–btn_out circuit connected
-            jacobs_ladder(False)
-
 
 if __name__=='__main__':
     button_led.on()
-    listen_to_button()
+    #listen_to_button()
+    button.when_pressed=jacobs_ladder
+    pause() #keep program running
 
